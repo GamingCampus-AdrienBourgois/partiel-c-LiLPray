b04ffcfb65a6c7b27bf30591a788b1ef5b5fea07
unknown <gandre@gamingcampus.local>
2024-01-19 13:40:40 +0100

    Ex2

--- StudentLibrary/Solution1.cpp
+++ StudentLibrary/Solution1.cpp
@@ -16,12 +16,10 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
     case TemperatureUnits::CELSIUS:
         if (_to == TemperatureUnits::FAHRENHEIT)
         {
-            // Celsius to Fahrenheit °F = 9/5 (°C) + 32
             return (_value * 9 / 5) + 32;
         }
         else if (_to == TemperatureUnits::KELVIN)
         {
-            // Celsius to Kelvin K = °C + 273
             return _value + 273;
         }
         break;
@@ -29,12 +27,10 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
     case TemperatureUnits::FAHRENHEIT:
         if (_to == TemperatureUnits::CELSIUS)
         {
-            // Fahrenheit to Celsius °C = 5/9 (°F - 32)
             return (_value - 32) * 5 / 9;
         }
         else if (_to == TemperatureUnits::KELVIN)
         {
-            // Fahrenheit to Kelvin K = 5/9 (°F - 32) + 273
             return (_value - 32) * 5 / 9 + 273;
         }
         break;
@@ -42,12 +38,10 @@ float Solution1::ConvertTemperature(float _value, TemperatureUnits _from, Temper
     case TemperatureUnits::KELVIN:
         if (_to == TemperatureUnits::CELSIUS)
         {
-            // Kelvin to Celsius °C = K - 273
             return _value - 273;
         }
         else if (_to == TemperatureUnits::FAHRENHEIT)
         {
-            // Kelvin to Fahrenheit °F = 9/5 (K - 273) + 32
             return (_value - 273) * 9 / 5 + 32;
         }
         break;
--- StudentLibrary/Solution2.cpp
+++ StudentLibrary/Solution2.cpp
@@ -1,14 +1,67 @@
+// Solution2.cpp
+
 #include "Solution2.h"
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
 
-#include <ostream>
-
-// Don't forget to enable the exercise in the SudentConfiguration.h file !
+// Don't forget to enable the exercise in the StudentConfiguration.h file!
 #include "StudentConfiguration.h"
 #ifdef COMPILE_EXERCICE_2
 
 float Solution2::GetBalance(const std::string& accountName)
 {
-	return -1.0f;
+    // Construire le chemin du fichier à partir du nom du compte
+    std::string filePath = "BankAccount/" + accountName + ".txt";
+
+    // Ouvrir le fichier
+    std::ifstream file(filePath);
+    if (!file.is_open())
+    {
+        // Si le fichier n'est pas trouvé, lancer une exception
+        throw std::runtime_error("File not found: " + filePath);
+    }
+
+    float balance = 0.0f;
+    std::string line;
+
+    // Lire chaque ligne du fichier
+    while (std::getline(file, line))
+    {
+        // Utiliser un flux de chaînes pour extraire l'opération (+ ou -) et le montant
+        std::istringstream iss(line);
+        char operation;
+        float amount;
+
+        if (iss >> operation >> amount)
+        {
+            // Mettre à jour le solde en fonction de l'opération
+            if (operation == '+')
+            {
+                balance += amount;
+            }
+            else if (operation == '-')
+            {
+                balance -= amount;
+            }
+            else
+            {
+                // Si l'opération n'est ni + ni -, lancer une exception
+                throw std::runtime_error("Invalid operation in file: " + filePath);
+            }
+        }
+        else
+        {
+            // Si la ligne ne contient pas correctement une opération et un montant, lancer une exception
+            throw std::runtime_error("Invalid line format in file: " + filePath);
+        }
+    }
+
+    // Fermer le fichier après lecture
+    file.close();
+
+    // Retourner le solde calculé
+    return balance;
 }
 
 #endif
