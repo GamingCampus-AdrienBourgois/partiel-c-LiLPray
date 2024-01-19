3a4fbf1606582ee314fdf14dea2701e3c5a6581f
unknown <gandre@gamingcampus.local>
2024-01-19 14:17:39 +0100

    Ex3

--- StudentLibrary/Solution3.cpp
+++ StudentLibrary/Solution3.cpp
@@ -1,4 +1,5 @@
 #include "Solution3.h"
+#include <algorithm>
 
 // Don't fortget to enable the exercise in the SudentConfiguration.h file !
 #include "StudentConfiguration.h"
@@ -9,7 +10,18 @@ void Solution3::SetWords(const std::vector<std::string>& _words)
 	words = _words;
 }
 
-void Solution3::SortWords() {}
+void Solution3::SortWords() 
+{
+    // Utilisez std::sort pour trier les mots en ordre alphabétique
+    std::sort(words.begin(), words.end(), [](const std::string& a, const std::string& b)
+        {
+            // Comparez les mots sans tenir compte de la casse
+            return std::lexicographical_compare(a.begin(), a.end(), b.begin(), b.end(), [](char c1, char c2)
+                {
+                    return std::tolower(c1) < std::tolower(c2);
+                });
+        });
+}
 
 std::vector<std::string> Solution3::GetSortedWords() const
 {
